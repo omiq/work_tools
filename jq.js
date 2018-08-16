520354cdaaa2a5b711fb8d18991a4fb58c0245de
omiq <chris@chrisg.com>
2018-08-15 22:55:34 -0500

    10 second refresh

--- jq.js
+++ jq.js
@@ -1,26 +1,32 @@
 
 
 
-function load_gallery($)
+function load_gallery()
 	{var url = 'http://geekahol.com/wp-json/wp/v2/posts?_embed';
 
 	jQuery.getJSON( url, function( data ) {
 		console.log(Date.now());
-$(".image_gal").empty();
+jQuery(".image_gal").empty();
 		data.forEach(function(item) {
   console.log(item);
 			var src_img = item['_embedded']['wp:featuredmedia'][0]['media_details']['sizes']['medium_large']['source_url'];
 console.log(src_img);
-  			$(".image_gal").append( "<img src='" + src_img + "'>" );
+  			jQuery(".image_gal").append( "<img src='" + src_img + "'>" );
 		})
 	});}
 
-setTimeout(function(){
-   load_gallery($);
-}, 25000);
 
-jQuery(document).ready(function ($) {
-	load_gallery($);
+
+function timeout() {
+    setTimeout(function () {
+        load_gallery();
+        timeout();
+    }, 10000);
+}
+
+jQuery(document).ready(function () {
+	load_gallery();
+	timeout();
 });
 
 
