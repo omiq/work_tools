49d93aaa9a8ada04ea223b88a2e43eedb3baf56d
omiq <chris@chrisg.com>
2018-08-20 15:25:05 -0600

    Initial Commit

---
+++ tweeter.py
@@ -0,0 +1,58 @@
+import os
+import re
+import sys
+import tweepy
+from tweepy import OAuthHandler
+from textblob import TextBlob
+
+"""
+This script grabs the most recent 200
+tweets containing eg. @studiopress and
+tests for sentiment.
+"""
+
+
+class TwitterClient(object):
+    '''
+    Generic Twitter Class for sentiment analysis.
+    '''
+
+    def __init__(self):
+        '''
+        Class constructor or initialization method.
+        '''
+        # keys and tokens from the Twitter Dev Console
+        consumer_key = os.environ['TWITTER_KEY']
+        consumer_secret = os.environ['TWITTER_SECRET']
+        access_token = os.environ['TWITTER_TOKEN']
+        access_token_secret = os.environ['TWITTER_TOKEN_SECRET']
+
+        # attempt authentication
+        try:
+            # create OAuthHandler object
+            self.auth = OAuthHandler(consumer_key, consumer_secret)
+            # set access token and secret
+            self.auth.set_access_token(access_token, access_token_secret)
+            # create tweepy API object to fetch tweets
+            self.api = tweepy.API(self.auth)
+        except:
+            print("Error: Authentication Failed")
+
+    def tweet(self, text):
+        self.api.update_status(text)
+
+
+def main():
+    # get the argument
+    txt = sys.argv[1]
+
+    # create TwitterClient object
+    api = TwitterClient()
+
+    # tweet
+    response = api.tweet(txt)
+
+
+if __name__ == "__main__":
+    # calling main function
+    main()
