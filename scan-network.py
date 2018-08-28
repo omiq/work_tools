d81d164c98423556335b50e883fa190202af3a7c
omiq <chris@chrisg.com>
2018-08-28 15:25:27 -0600

    Better output

--- scan-network.py
+++ scan-network.py
@@ -1,14 +1,23 @@
 import sys
 import nmap
+import time
 nm = nmap.PortScanner()
 nm.scan('10.0.1.0/24', '22')
 
+# clear screen
+sys.stdout.write(u"\u001b[2J\u001b[0;0H")
+sys.stdout.flush()
+time.sleep(0.2)
+
+# iterate over the hosts on the network
 for host in sorted(nm.all_hosts()):
 
+    # blank if not Pi
     vendor = ""
 
     if len(nm[host]['vendor']) > 0:
 
+        # attempt to get mac address
         try:
             mac = nm[host]['addresses']['mac']
         except:
@@ -17,8 +26,8 @@ for host in sorted(nm.all_hosts()):
         vendor = tuple(nm[host]['vendor'].values())
         if str(vendor).find('Raspberry') > 0:
             vendor = "Pi!"
-            sys.stdout.write(u"\u001b[41;1m" + "{} ({}) {}\n\u001b[0m".format(nm[host].hostname(), host, mac))
+            print(u"\u001b[37m" + "{} ({}) {}\u001b[0m".format(nm[host].hostname(), host, mac))
         else:
             vendor = ""
-            sys.stdout.write(u"\u001b[40;1m" + "{} ({}) {}\n\u001b[0m".format(nm[host].hostname(), host, mac))
+            print(u"\u001b[0m" + "{} ({}) {}\u001b[0m".format(nm[host].hostname(), host, mac))
 
